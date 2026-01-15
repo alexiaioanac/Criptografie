32c0da061dcc8f914fa43110e4dd6cd071c62314
Alexia <alexiaioana1225@gmail.com>
2026-01-15 16:53:12 +0200

    Add project files.

---
+++ Functii hash criptografice/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Win32;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Functii_hash_criptografice
+{
+    public partial class MainWindow : Window
+    {
+        SimpleHash sh = new SimpleHash();
+
+        public MainWindow() => InitializeComponent();
+
+        private void SelectFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == true) InputPath.Text = ofd.FileName;
+        }
+
+        private void Radio_Checked(object sender, RoutedEventArgs e)
+        {
+            if (sender is System.Windows.Controls.RadioButton rb)
+                if (rb?.Tag != null)
+                {
+                    sh.hashIdx = int.Parse(rb.Tag.ToString());
+                }
+        }
+
+        private async void Calculate_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                await sh.HashFileAsync(InputPath.Text, "rezultat_hash.txt");
+                MessageBox.Show("Gata! Verifică fișierul rezultat_hash.txt");
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+        }
+    }
+}
---
+++ Functii hash criptografice/SimpleHash.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Functii_hash_criptografice
+{
+    internal class SimpleHash
+    {
+        internal int hashIdx = 0;
+
+        internal async Task HashFileAsync(string inputPath, string outputPath)
+        {
+            if (string.IsNullOrWhiteSpace(inputPath) || !File.Exists(inputPath))
+                throw new Exception("Fisierul de intrare nu este valid!");
+
+           
+            HashAlgorithm alg = hashIdx switch
+            {
+                0 => MD5.Create(),
+                1 => SHA1.Create(),
+                2 => SHA256.Create(),
+                3 => SHA384.Create(),
+                4 => SHA512.Create(),
+                5 => SHA3_256.Create(),
+                6 => SHA3_384.Create(),
+                7 => SHA3_512.Create(),
+                _ => MD5.Create()
+            };
+
+            using (alg)
+            {
+                using var stream = File.OpenRead(inputPath);
+                
+                byte[] hashBytes = await alg.ComputeHashAsync(stream);
+
+               
+                StringBuilder sb = new StringBuilder();
+                foreach (byte b in hashBytes) sb.Append(b.ToString("x2"));
+
+                await File.WriteAllTextAsync(outputPath, sb.ToString());
+            }
+        }
+    }
+}
