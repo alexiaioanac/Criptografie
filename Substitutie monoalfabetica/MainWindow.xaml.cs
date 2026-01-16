ccc94c265814bfc40f0d5f98ec4db021d6acc344
Alexia <alexiaioana1225@gmail.com>
2026-01-16 20:18:31 +0200

    Substitutie monoalfabetica

---
+++ Substitutie monoalfabetica/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Substitutie_monoalfabetica
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private void BtnCriptare_Click(object sender, RoutedEventArgs e)
+        {
+            TxtResult.Text = Substitutie.Encrypt(TxtInput.Text);
+        }
+
+        private void BtnDecriptare_Click(object sender, RoutedEventArgs e)
+        {
+            TxtResult.Text = Substitutie.Decrypt(TxtInput.Text);
+        }
+
+        private void BtnAnaliza_Click(object sender, RoutedEventArgs e)
+        {
+            TxtResult.Text = Substitutie.AnalizaFrecventa(TxtInput.Text);
+        }
+    }
+}
---
+++ Substitutie monoalfabetica/Substitutie.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Substitutie_monoalfabetica
+{
+    internal class Substitutie
+    {
+       
+            private static string alfabet = "abcdefghijklmnopqrstuvwxyz";
+            private static string cheie = "mipodsqrabcfghjklntuvwxyz";
+
+            public static string Encrypt(string plain)
+            {
+                if (string.IsNullOrWhiteSpace(plain)) return plain;
+                StringBuilder sb = new StringBuilder();
+
+                foreach (char c in plain.ToLower())
+                {
+                    int index = alfabet.IndexOf(c);
+                    if (index != -1) sb.Append(cheie[index]);
+                    else sb.Append(c); 
+                }
+                return sb.ToString();
+            }
+
+            public static string Decrypt(string cipher)
+            {
+                StringBuilder sb = new StringBuilder();
+                foreach (char c in cipher.ToLower())
+                {
+                    int index = cheie.IndexOf(c);
+                    if (index != -1) sb.Append(alfabet[index]);
+                    else sb.Append(c);
+                }
+                return sb.ToString();
+            }
+
+            public static string AnalizaFrecventa(string cipher)
+            {
+                var frecventa = cipher.ToLower()
+                    .Where(c => char.IsLetter(c))
+                    .GroupBy(c => c)
+                    .OrderByDescending(g => g.Count());
+
+                StringBuilder sb = new StringBuilder();
+                sb.AppendLine("Frecvența literelor detectate:");
+                foreach (var grup in frecventa)
+                {
+                    sb.AppendLine($"Litera '{grup.Key}' apare de {grup.Count()} ori.");
+                }
+                return sb.ToString();
+            }
+        
+    }
+}
